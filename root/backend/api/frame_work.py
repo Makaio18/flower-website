1c4995748918961fa549604cf51ccd3f2b97d70b
Kai Villarreal <158221000+Makaio18@users.noreply.github.com>
2026-08-09 22:21:58 -0700

    Fix: removed submodule tracking and added normal files

---
+++ root/backend/api/frame_work.py
@@ -0,0 +1,12 @@
+#Adriel Makaio Villarreal
+"""
+Frame work for the the indvidual data base holding the html data, and how I am going to populate the infromation
+"""
+import requests
+
+response = requests.get()
+
+
+# Hi I am just testing if this is making it to my git hub repo
+
+
---
+++ root/data/frame_work.py
@@ -0,0 +1,9 @@
+#Adriel Makaio Villarreal
+"""
+Frame work for the the indvidual data base holding the html data, and how I am going to populate the infromation
+"""
+import requests
+
+response = requests.get()
+
+
