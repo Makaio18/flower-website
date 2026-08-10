fe934c610c69c014d48bcaad3468d6155d688abc
Kai Villarreal <158221000+Makaio18@users.noreply.github.com>
2026-08-09 22:38:44 -0700

    changes to file strucutre

--- application/data/frame_work.py
+++
@@ -1,9 +0,0 @@
-#Adriel Makaio Villarreal
-"""
-Frame work for the the indvidual data base holding the html data, and how I am going to populate the infromation
-"""
-import requests
-
-response = requests.get()
-
-
